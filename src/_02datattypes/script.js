821acd51354d70422750fe41f47e6970f927d154
Tutku Ince <tutku.ince@outlook.com>
2023-06-01 10:37:06 +0300

    data types

---
+++ src/_02datattypes/script.js
@@ -0,0 +1,35 @@
+// number
+var x = 10.6;
+console.log(typeof x);
+
+// big int
+var x = 1000n;
+console.log(typeof x);
+
+// string
+var x = 'Tutan';
+console.log(typeof x);
+
+// boolean
+var x = true;
+console.log(typeof x);
+
+// undefined
+var x;
+console.log(typeof x);
+
+// objects
+var obj = {};
+console.log(typeof obj);
+
+var arr = [];
+console.log(arr);
+
+// symbol
+var sym = Symbol('id');
+console.log(typeof sym);
+
+// null 
+var x = 10;
+x = null;
+console.log(typeof x);
