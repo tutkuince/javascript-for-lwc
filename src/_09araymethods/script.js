9771ca728ee43781a7b545b84c97adead3d16fc6
Tutku Ince <tutku.ince@outlook.com>
2023-06-06 21:25:33 +0300

    array methods

--- src/_09araymethods/script.js
+++ src/_09araymethods/script.js
@@ -24,6 +24,7 @@ let arr = [2, 3, 5, 7, 9, 10, 8, 6];
 // console.log(`ResultEvery: ${resultEvery}`);
 
 // some()   - return true if at least one element in the array satisfy 
+//
 // let resultSome = arr.some((value, index, array) => {
 //     return value > 2;
 // });
@@ -31,12 +32,23 @@ let arr = [2, 3, 5, 7, 9, 10, 8, 6];
 
 
 // sort()   - sort the elements of an array
+//
 // let resultSort = arr.sort((a, b) => {
 //     return a - b;       // order by asc
 //     // return b - a;    // order by desc
 // });
 // console.log(resultSort);
 
-// reduce() - this method reduces the array to a single value (left to right)
+// reduce() - this method reduces the array to a single value (left to right) // we can use reduce method for addition    // 0 => initialValue
+//
+// let resultReduce = arr.reduce((previousValue, currentValue, currentIndex, array) => {
+//     return previousValue + currentValue;
+// }, 0);
+// console.log(`resultReduce ${resultReduce}`);
+
 
 // forEach()- this method calls for each array element
+//
+arr.forEach((value, index, array) => {
+    console.log(value);
+});
